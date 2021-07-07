7eadf17bb8e9f44d05fedf26546b8e97baf9dc4a
fernal73 <linus.fernandes@gmail.com>
2021-07-07 17:41:38 +0530

    Add more exceptions

--- exceptions.d
+++ exceptions.d
@@ -2,6 +2,13 @@
 import std.file : FileException, readText;
 import std.stdio : writeln;
 
+void dangerousOperation() {
+  import std.exception : enforce;
+
+  float magic = 1_000_000_000;
+  enforce(magic + 42 - magic == 42, "Floating-point math is fun");
+}
+
 void main() {
   try {
     readText("dummyFile");
@@ -12,4 +19,16 @@ void main() {
     writeln("Stack trace:\n", e.info);
     writeln(e);
   }
+  import std.exception : collectException;
+
+  auto e = collectException(dangerousOperation());
+  if (e)
+    writeln("The dangerous operation failed with ", e);
+  writeln(lessThan(10, 5));
+}
+
+bool lessThan(int a, int b) nothrow
+{
+   // writeln("unsafe world"); // output can throw exceptions, thus this is forbidden
+    return a < b;
 }
